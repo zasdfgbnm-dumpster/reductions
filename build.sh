d2715ae040e990d7b3473714ee39c665c1bf9729
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2021-09-08 15:01:20 -0700

    update script

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-st=`date +%s%N`
-
 time nvcc -c main.cu \
     -std=c++17 \
+    --keep -v \
     -I . \
-    -D__CUDA_NO_HALF_OPERATORS__ \
-    -D__CUDA_NO_BFLOAT16_OPERATORS__
-
-et=`date +%s%N` 
-compt=`echo "scale=3;($et-$st)/1000000000" | bc -l`
-echo 'wall time' $compt 'second'
+    -D __CUDA_NO_HALF_OPERATORS__ \
+    -D __CUDA_NO_BFLOAT16_OPERATORS__
